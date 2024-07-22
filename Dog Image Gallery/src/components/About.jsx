c4383ff4558e68b2f6dc8cf80f0be09a6d0efc5d
kyhol <Kyle.Hollett@keyin.com>
2024-07-22 20:40:03 -0230

    Added an about page that needs to be updated with descriptions of what is used in each component + installed packages

--- Dog Image Gallery/src/components/About.jsx
+++ Dog Image Gallery/src/components/About.jsx
@@ -1,5 +1,90 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
+import { IoIosArrowDropdown } from "react-icons/io";
 const About = () => {
-  return <></>;
+  const [showInfo, setShowInfo] = useState({
+    header: false,
+    imageGallery: false,
+    breedSelector: false,
+    dogCarousel: false,
+    footer: false,
+    about: false,
+    packages: false,
+  });
+
+  const toggleInfo = (section) => {
+    setShowInfo((prevState) => ({
+      ...prevState,
+      [section]: !prevState[section],
+    }));
+  };
+
+  return (
+    <div className="aboutPageWrapper">
+      <h1>
+        Header{" "}
+        <span onClick={() => toggleInfo("header")} className="dropDownArrow">
+          <IoIosArrowDropdown />
+        </span>
+      </h1>
+      {showInfo.header && <p>Header Info</p>}
+
+      <h1>
+        Image Gallery{" "}
+        <span
+          className="dropDownArrow"
+          onClick={() => toggleInfo("imageGallery")}
+        >
+          <IoIosArrowDropdown />
+        </span>
+      </h1>
+      {showInfo.imageGallery && <p>Image Gallery info</p>}
+
+      <h1>
+        Breed Selector{" "}
+        <span
+          className="dropDownArrow"
+          onClick={() => toggleInfo("breedSelector")}
+        >
+          <IoIosArrowDropdown />
+        </span>
+      </h1>
+      {showInfo.breedSelector && <p>Breed Selector Info</p>}
+
+      <h1>
+        Dog Carousel{" "}
+        <span
+          className="dropDownArrow"
+          onClick={() => toggleInfo("dogCarousel")}
+        >
+          <IoIosArrowDropdown />
+        </span>
+      </h1>
+      {showInfo.dogCarousel && <p>Dog Carousel Info</p>}
+
+      <h1>
+        Footer{" "}
+        <span className="dropDownArrow" onClick={() => toggleInfo("footer")}>
+          <IoIosArrowDropdown />
+        </span>
+      </h1>
+      {showInfo.footer && <p>Footer info</p>}
+
+      <h1>
+        About{" "}
+        <span className="dropDownArrow" onClick={() => toggleInfo("about")}>
+          <IoIosArrowDropdown />
+        </span>
+      </h1>
+      {showInfo.about && <p>About info</p>}
+      <h1>
+        Installed Packages{" "}
+        <span className="dropDownArrow" onClick={() => toggleInfo("packages")}>
+          <IoIosArrowDropdown />
+        </span>
+      </h1>
+      {showInfo.packages && <p>Installed packages info</p>}
+    </div>
+  );
 };
+
+export default About;
--- Dog Image Gallery/src/components/Header.jsx
+++ Dog Image Gallery/src/components/Header.jsx
@@ -1,26 +1,28 @@
+// src/components/Header.js
 import React from "react";
 import { GiDogHouse } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <>
-      <div className="header">
-        <div className="headerButtonWrapper">
-          <button className="headerButton">
-            <Link to="/">Home</Link> &nbsp;
-            <GiDogHouse />
-          </button>
-          <button className="headerButton">
-            {" "}
-            <Link to="/about">About</Link>
-          </button>
-          <a href="https://dog.ceo/dog-api/" target="blank">
-            <button className="headerButton">Dog API</button>
-          </a>
-        </div>
+    <div className="header">
+      <div className="headerButtonWrapper">
+        <button className="headerButton">
+          <Link to="/">Home</Link> &nbsp;
+          <GiDogHouse />
+        </button>
+        <button className="headerButton">
+          <Link to="/about">About</Link>
+        </button>
+        <a
+          href="https://dog.ceo/dog-api/"
+          target="_blank"
+          rel="noopener noreferrer" // Added noopener noreferrer for security
+        >
+          <button className="headerButton">Dog API</button>
+        </a>
       </div>
-    </>
+    </div>
   );
 };
 
